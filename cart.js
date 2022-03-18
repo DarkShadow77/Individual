c5da2f18af4661530d2f7e2048e11d6df40b03f6
David Adeshina <dadeshina4@gmail.com>
2022-03-18 20:38:15 +0100

    Initial commit

---
+++ cart.js
@@ -0,0 +1,10 @@
+let lessons = [
+    {
+        'id': 1,
+        'subject': "Math",
+        'location': "London",
+        'price': 100,
+        'Numberofspaces': 5, 
+        'images': "images/math.png",
+    },
+]
---
+++ lesson.js
@@ -0,0 +1,42 @@
+let lessons = [
+    {
+        'id': 1,
+        'subject': "Math",
+        'location': "London",
+        'price': 49,
+        'Numberofspaces': 5, 
+        'image': "images/math.png",
+    },
+    {
+        'id': 2,
+        'subject': "English",
+        'location': "London",
+        'price': 30,
+        'Numberofspaces': 5, 
+        'image': "images/math.png",
+    },
+    {
+        'id': 3,
+        'subject': "Physics",
+        'location': "London",
+        'price': 75,
+        'Numberofspaces': 5, 
+        'image': "images/math.png",
+    },
+    {
+        'id': 4,
+        'subject': "Chemistry",
+        'location': "London",
+        'price': 60,
+        'Numberofspaces': 5, 
+        'image': "images/math.png",
+    },
+    {
+        'id': 5,
+        'subject': "Biology",
+        'location': "London",
+        'price': 40,
+        'Numberofspaces': 5, 
+        'image': "images/math.png",
+    },
+]
